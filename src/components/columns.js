01a6b859cb681c267b48dd7167bc6d307044213a
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-03-14 22:13:15 +0000

    added data formatting

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,3 +1,5 @@
+import format from "date-fns/format"
+
 export const COLUMNS = [
   {
     Header: 'Id',
@@ -22,7 +24,8 @@ export const COLUMNS = [
   {
     Header: 'Date of Birth',
     Footer: 'Date of Birth',
-    accessor: 'date_of_birth'
+    accessor: 'date_of_birth',
+    Cell: ({value}) => format(new Date(value), 'dd/MM/yyyy')
   },
   {
     Header: 'Country',
